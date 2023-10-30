a58c64029588b9f9cc78cd7bd93877a34f1fd598
atul-settyl <atul.morchhlay@settyl.com>
2023-10-30 18:34:35 +0530

    Fixed delete project api

--- server/src/controllers/project/delete-project.ts
+++ server/src/controllers/project/delete-project.ts
@@ -8,10 +8,39 @@ export async function deleteProject(req: Request, res: Response) {
     const projectId = req.params.projectId;
 
     console.log(parseInt(projectId), projectId);
-    const deletedProject = await prisma.project.delete({
+    const targetProject = await prisma.project.findFirst({
       where: {
         id: parseInt(projectId) || 0,
         userId: req.currentUser.id,
+      },
+    });
+    if (!targetProject) {
+      dbError(res, "Project not found.");
+      return;
+    }
+
+    const targetApis = await prisma.api.findMany({
+      where: {
+        projectId: targetProject.id,
+      },
+    });
+
+    if (targetApis.length > 0) {
+      await prisma.api_model.deleteMany({
+        where: {
+          apiId: { in: targetApis.map((a) => a.id) },
+        },
+      });
+      await prisma.api.deleteMany({
+        where: {
+          id: { in: targetApis.map((a) => a.id) },
+        },
+      });
+    }
+
+    const deletedProject = await prisma.project.delete({
+      where: {
+        id: targetProject.id,
       },
     });
 
