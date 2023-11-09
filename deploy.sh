9aee5daff2703dc4e719717ae4c8d9387b4bf0fd
atul-settyl <atul.morchhlay@settyl.com>
2023-11-09 15:46:33 +0530

    containerised fronend

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,13 @@ export PATH=$PATH:/root/.nvm/versions/node/v20.9.0/bin
 
 POSTGRESS_DATABASE_PASSWORD="$1"
 BACKEND_IMAGE_NAME="api-generator-backend"
+FRONTEND_IMAGE_NAME="api-generator-frontend"
 DATABASE_URL="postgres://postgres:$POSTGRESS_DATABASE_PASSWORD@postgres_database:5432"
 IMAGE_TAG="latest"
 DATABASE_NETWORK="postgres_bridge"
 PORTS=(8001 8002)
 DATABASE_NAME="api_generator"
+FRONTEND_PORT=3001
  
 cd ~/api-generator
 git pull origin main
@@ -19,8 +21,10 @@ else
   exit 1
 fi
 
+
+# back-end 
+echo "Running backend build"
 cd ~/api-generator/server
-npm install
 
 for PORT in "${PORTS[@]}"
 do
@@ -30,7 +34,6 @@ done
 
 docker rmi atul24112001/$BACKEND_IMAGE_NAME:$IMAGE_TAG
 docker build -t atul24112001/$BACKEND_IMAGE_NAME:$IMAGE_TAG .
-docker build -t $BACKEND_IMAGE_NAME:$IMAGE_TAG .
 
 if [ $? -eq 0 ]; then
   echo "Docker image atul24112001/$BACKEND_IMAGE_NAME:$IMAGE_TAG built successfully."
@@ -44,12 +47,20 @@ do
   docker run -e DATABASE_URL=$DATABASE_URL/$DATABASE_NAME --name $BACKEND_IMAGE_NAME-$PORT --network $DATABASE_NETWORK -d -p $PORT:8000 atul24112001/$BACKEND_IMAGE_NAME:$IMAGE_TAG
 done
 
+# Front-end 
+echo "Running fronend build"
 cd ~/api-generator/frontend
-npm install
-rm -r .next
-npm install
-npm run build
+docker stop $FRONTEND_IMAGE_NAME
+docker rm $FRONTEND_IMAGE_NAME
+docker rmi atul24112001/$FRONTEND_IMAGE_NAME:$IMAGE_TAG
+docker build -t atul24112001/$FRONTEND_IMAGE_NAME:$IMAGE_TAG .
 
-pm2 restart api-generator-frontend 
+if [ $? -eq 0 ]; then
+  echo "Docker image atul24112001/$FRONTEND_IMAGE_NAME:$IMAGE_TAG built successfully."
+else
+  echo "Docker image build failed."
+  exit 1
+fi
+docker run -d -p $FRONTEND_PORT:3000 atul24112001/$BACKEND_IMAGE_NAME:$IMAGE_TAG
 
 echo "Build Successfully."
