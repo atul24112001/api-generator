1a9bee829e4eac2dd4e55c0f6fc0cfbab26a9720
atul-settyl <atul.morchhlay@settyl.com>
2023-11-09 13:58:29 +0530

    Test path

--- deploy.sh
+++ deploy.sh
@@ -9,47 +9,51 @@ DATABASE_NETWORK="postgres_bridge"
 PORTS=(8001 8002)
 DATABASE_NAME="api_generator"
 
-git pull origin main
-cd ~/api-generator/server
-npm install
-cd ~/api-generator/frontend
-npm install
-cd ~/api-generator
+pwd
+ls
 
-if [ $? -eq 0 ]; then
-  echo "Git backend pull successful."
-else
-  echo "Git backend pull failed. Aborting deployment."
-  exit 1
-fi
+# git pull origin main
+# cd ~/server
+# npm install
+# cd ~/frontend
+# npm install
+# cd ~
 
-cd ~/api-generator/server
-for PORT in "${PORTS[@]}"
-do
-  docker stop $BACKEND_IMAGE_NAME-$PORT
-  docker rm $BACKEND_IMAGE_NAME-$PORT
-done
+# if [ $? -eq 0 ]; then
+#   echo "Git backend pull successful."
+# else
+#   echo "Git backend pull failed. Aborting deployment."
+#   exit 1
+# fi
 
-docker rmi atul24112001/$BACKEND_IMAGE_NAME:$IMAGE_TAG
-docker build -t atul24112001/$BACKEND_IMAGE_NAME:$IMAGE_TAG .
-IMAGE_ID=$(docker build -t $BACKEND_IMAGE_NAME:$IMAGE_TAG . | tail -n 1 | awk '{print $3}')
+# cd ~/server
+# for PORT in "${PORTS[@]}"
+# do
+#   docker stop $BACKEND_IMAGE_NAME-$PORT
+#   docker rm $BACKEND_IMAGE_NAME-$PORT
+# done
 
-if [ $? -eq 0 ]; then
-  echo "Docker image atul24112001/$BACKEND_IMAGE_NAME:$IMAGE_TAG built successfully."
-else
-  echo "Docker image build failed."
-  exit 1
-fi
+# docker rmi atul24112001/$BACKEND_IMAGE_NAME:$IMAGE_TAG
+# docker build -t atul24112001/$BACKEND_IMAGE_NAME:$IMAGE_TAG .
+# IMAGE_ID=$(docker build -t $BACKEND_IMAGE_NAME:$IMAGE_TAG . | tail -n 1 | awk '{print $3}')
 
-for PORT in "${PORTS[@]}"
-do
-  docker run -e DATABASE_URL=$DATABASE_URL/$DATABASE_NAME --name $BACKEND_IMAGE_NAME-$PORT --network $DATABASE_NETWORK -d -p $PORT:8000 $IMAGE_ID
-done
+# if [ $? -eq 0 ]; then
+#   echo "Docker image atul24112001/$BACKEND_IMAGE_NAME:$IMAGE_TAG built successfully."
+# else
+#   echo "Docker image build failed."
+#   exit 1
+# fi
 
-cd ~/api-generator/frontend
-rm -r .next
-npm run build
+# for PORT in "${PORTS[@]}"
+# do
+#   docker run -e DATABASE_URL=$DATABASE_URL/$DATABASE_NAME --name $BACKEND_IMAGE_NAME-$PORT --network $DATABASE_NETWORK -d -p $PORT:8000 $IMAGE_ID
+# done
 
-pm2 restart api-generator-frontend 
+# cd ~/frontend
+# rm -r .next
+# npm install
+# npm run build
+
+# pm2 restart api-generator-frontend 
 
 echo "Build Successfully."
