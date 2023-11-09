c77ef9e2c2a1ce117eb14a3c09255784b2692ea1
atul-settyl <atul.morchhlay@settyl.com>
2023-11-09 13:51:08 +0530

    Updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 export PATH=$PATH:/root/.nvm/versions/node/v16.14.2/bin
 
+POSTGRESS_DATABASE_PASSWORD="$1"
 BACKEND_IMAGE_NAME="api-generator-backend"
 DATABASE_URL="postgres://postgres:$POSTGRESS_DATABASE_PASSWORD@postgres_database:5432"
 IMAGE_TAG="latest"
@@ -29,9 +30,9 @@ do
   docker rm $BACKEND_IMAGE_NAME-$PORT
 done
 
-docker rmi atul24112001/$BACKEND_IMAGE_NAME
+docker rmi atul24112001/$BACKEND_IMAGE_NAME:$IMAGE_TAG
 docker build -t atul24112001/$BACKEND_IMAGE_NAME:$IMAGE_TAG .
-cd ~/api-generator
+IMAGE_ID=$(docker build -t $BACKEND_IMAGE_NAME:$IMAGE_TAG . | tail -n 1 | awk '{print $3}')
 
 if [ $? -eq 0 ]; then
   echo "Docker image atul24112001/$BACKEND_IMAGE_NAME:$IMAGE_TAG built successfully."
@@ -42,13 +43,12 @@ fi
 
 for PORT in "${PORTS[@]}"
 do
-  docker run -e DATABASE_URL=$DATABASE_URL/$DATABASE_NAME --name $BACKEND_IMAGE_NAME-$PORT --network $DATABASE_NETWORK -d -p $PORT:8000 atul24112001/$BACKEND_IMAGE_NAME:$IMAGE_TAG
+  docker run -e DATABASE_URL=$DATABASE_URL/$DATABASE_NAME --name $BACKEND_IMAGE_NAME-$PORT --network $DATABASE_NETWORK -d -p $PORT:8000 $IMAGE_ID
 done
 
 cd ~/api-generator/frontend
 rm -r .next
 npm run build
-cd ~/api-generator
 
 pm2 restart api-generator-frontend 
 
